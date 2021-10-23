aa001d751b269f4dbd37d527de387605ba442496
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2021-10-24 01:23:38 +0530

    add: home page component done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import "remixicon/fonts/remixicon.css";
 
 const Header = () => {
+  // const [mediaQueryEnabled, setMediaQueryEnabled] = useState(false);
+  const onMenuClick = () => {
+    document.querySelector(".menu-container").classList.add("active");
+  };
+
+  const onCloseClick = () => {
+    document.querySelector(".menu-container").classList.remove("active");
+  };
+
   return (
     <Container>
       <div className="header-menu left">
@@ -18,10 +28,139 @@ const Header = () => {
       <div className="header-menu right">
         <div className="shop">shop</div>
         <div className="account">account</div>
-        <div className="menu">menu</div>
+        <div
+          className="menu"
+          onClick={() => {
+            onMenuClick();
+          }}
+        >
+          menu
+        </div>
       </div>
       <div className="menu-container">
-        <div className="overlay"></div>
+        <div
+          className="overlay"
+          onClick={() => {
+            onCloseClick();
+          }}
+        ></div>
+        <div className="menu-items ">
+          <div className="close-btn">
+            <i
+              class="ri-close-fill"
+              onClick={() => {
+                onCloseClick();
+              }}
+            ></i>
+          </div>
+          <div className="menu-item-container">
+            <li>
+              <a href="#" className="menu-item">
+                model S
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                model 3
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                model x
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                model y
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                solar roof
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                solar panels
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Existing Inventory
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Used Intentory
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Trade In
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Test Drive
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Cybertruck
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Roadster
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Roadster
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Semi
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Charging
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                PowerWall
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Commercial energy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Utilities
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Find us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Support
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Inventory Relations
+              </a>
+            </li>
+          </div>
+        </div>
       </div>
     </Container>
   );
@@ -55,6 +194,7 @@ const Container = styled.div`
       display: flex;
       width: max-content;
       column-gap: 20px;
+      color: var(--font-color);
 
       div {
         font-size: var(--font-size-upper);
@@ -67,7 +207,112 @@ const Container = styled.div`
         transition: all 0.3s ease;
 
         &:hover {
-          background-color: var(--light-gray);
+          background-color: rgba(0, 0, 0, 0.1);
+        }
+      }
+    }
+  }
+
+  .menu-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    visibility: hidden;
+
+    &.active {
+      display: flex;
+      flex-direction: column;
+      visibility: visible;
+
+      .overlay {
+        width: 100vw;
+        height: 100vh;
+        backdrop-filter: blur(3px);
+        background-color: rgba(0, 0, 0, 0.3);
+      }
+
+      .menu-items {
+        position: absolute;
+        right: 0;
+        transform: translateX(0);
+        width: 270px;
+        height: 100vh;
+        animation: slideIn 0.3s;
+        background-color: white;
+
+        @keyframes slideIn {
+          0% {
+            transform: translateX(100%);
+          }
+          100% {
+            transform: translateX(0%);
+          }
+        }
+
+        .close-btn {
+          height: 80px;
+          background-color: white;
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+          padding-right: 30px;
+          font-size: 1.5rem;
+          font-weight: 700;
+          color: var(--font-color);
+          cursor: pointer;
+
+          i {
+            padding: 10px;
+            border-radius: 50%;
+            transition: all 0.3s ease-out;
+
+            &:hover {
+              background-color: var(--light-gray);
+            }
+          }
+        }
+
+        .menu-item-container {
+          height: calc(100vh - 100px);
+          width: 100%;
+          overflow-y: auto;
+          z-index: 110;
+          padding-bottom: 100px;
+
+          &::-webkit-scrollbar {
+            width: 5px;
+          }
+
+          &::-webkit-scrollbar-thumb {
+            background-color: var(--scrollbar-color);
+            border-radius: 8px;
+          }
+
+          &::-webkit-scrollbar-track {
+            background-color: transparent;
+          }
+
+          li {
+            height: 45px;
+            padding-left: 30px;
+            padding-right: 30px;
+
+            a {
+              font-size: var(--font-size-normal);
+              font-weight: 600;
+              height: 45px;
+              display: flex;
+              align-items: center;
+              padding-left: 20px;
+              text-transform: capitalize;
+              border-radius: 12px;
+              transition: all 0.3s ease-out;
+
+              &:hover {
+                background-color: var(--light-gray);
+              }
+            }
+          }
         }
       }
     }
---
+++ src/Components/Section.js
@@ -0,0 +1,205 @@
+import React from "react";
+import styled from "styled-components";
+import "remixicon/fonts/remixicon.css";
+
+const Section = (props) => {
+  return (
+    <Container>
+      <Wrapper src={props.img}>
+        <TitleContainer>
+          <div className="title">{props.title}</div>
+          <div className="desc">
+            {props.desc} <span>{props.span}</span>
+          </div>
+        </TitleContainer>
+        <BtnContainer>
+          {!props.button2 ? (
+            <div className="btns alone">
+              <div className="left btn">{props.button1}</div>
+            </div>
+          ) : (
+            <div className="btns">
+              <div className="left btn">{props.button1}</div>
+              <div className="right btn">{props.button2}</div>
+            </div>
+          )}
+          {props.index === 0 ? (
+            <div className="go-down-arrow">
+              <i class="ri-arrow-down-s-line"></i>
+            </div>
+          ) : (
+            ""
+          )}
+        </BtnContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Section;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  background-repeat: no-repeat;
+  object-fit: contain;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+`;
+
+const BtnContainer = styled.div`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+
+  .btns {
+    display: flex;
+    justify-content: space-between;
+    column-gap: 20px;
+    align-items: center;
+
+    &.alone {
+      display: flex;
+      justify-content: center;
+    }
+
+    .btn {
+      width: 70%;
+      background-color: orange;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: var(--font-size-small);
+      font-weight: 600;
+      text-transform: uppercase;
+      border-radius: 50px;
+      padding: 15px 0px;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+
+      &:hover {
+        transform: scale(1.02) translateY(-5px);
+      }
+
+      &.left {
+        background-color: #333439;
+        color: white;
+      }
+
+      &.right {
+        background-color: #e0dddc;
+      }
+    }
+  }
+
+  .go-down-arrow {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+
+    i {
+      font-size: 2.5rem;
+      color: gray;
+      animation: arrowDown 1.5s infinite;
+    }
+
+    @keyframes arrowDown {
+      0%,
+      20%,
+      50%,
+      80%,
+      100% {
+        transform: translateY(0px);
+      }
+
+      40% {
+        transform: translateY(5px);
+      }
+
+      60% {
+        transform: translateY(5px);
+      }
+    }
+  }
+
+  @media only screen and (max-width: 630px) {
+    width: 400px;
+  }
+
+  @media only screen and (max-width: 430px) {
+    width: 300px;
+
+    .btns {
+      flex-direction: column;
+      row-gap: 10px;
+
+      .btn {
+        padding: 8px 0px;
+        font-size: var(--font-size-x-small);
+      }
+    }
+  }
+
+  @media only screen and (max-width: 340px) {
+    width: 250px;
+  }
+`;
+
+const TitleContainer = styled.div`
+  margin-top: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .title {
+    font-size: var(--font-size-heading);
+    font-weight: 700;
+    text-transform: capitalize;
+    color: var(--font-color);
+    letter-spacing: 1.6px;
+  }
+
+  .desc {
+    margin-top: 5px;
+    font-size: var(--font-size-upper);
+    font-weight: 500;
+    color: var(--font-color-subHeading);
+
+    span {
+      border-bottom: 1px solid var(--font-color-subHeading);
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+
+      &:hover {
+        border-bottom: 2px solid var(--font-color-subHeading);
+        color: var(--font-color);
+      }
+    }
+  }
+
+  @media only screen and (max-width: 425px) {
+    margin-top: 100px;
+
+    .title {
+      font-size: var(--font-size-heading-res);
+      letter-spacing: 0px;
+    }
+
+    .desc {
+      font-size: var(--font-size-small);
+    }
+  }
+`;
